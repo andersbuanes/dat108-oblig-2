3834aa3740648e61cca4398d8728b4fcae7cdae7
andersbuanes <anderstbuanes@hotmail.com>
2020-09-11 12:22:53 +0200

    Oppgave 3 b

--- src/no/hvl/dat108/oppgave2/Oppg2.java
+++ src/no/hvl/dat108/oppgave2/Oppg2.java
@@ -1,34 +1,46 @@
 package no.hvl.dat108.oppgave2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
+import no.hvl.dat108.oppgave3.Ansatt;
+import no.hvl.dat108.oppgave3.Kjonn;
+
 public class Oppg2 {
 
 	public static void main(String[] args) {
-		List<Ansatt> ansatte = Arrays.asList();
-		
+		List<Ansatt> ansatte = new ArrayList<Ansatt>();
+
+		ansatte.add(new Ansatt("Mats", "Ramstad", "Kaptein", Kjonn.MANN, 725000));
+		ansatte.add(new Ansatt("Andreas", "Sannesmoen", "Vaktmester", Kjonn.MANN, 1000000));
+		ansatte.add(new Ansatt("Matilde", "Knarvik", "Matros", Kjonn.KVINNE, 450000));
+		ansatte.add(new Ansatt("Henrik", "Hammer", "Sjef for memes", Kjonn.MANN, 550000));
+		ansatte.add(new Ansatt("Andrea", "Sannesmoen", "Sjef for økomoni", Kjonn.KVINNE, 650000));
+
 		skrivUtAlle(ansatte);
-		
+
 		lonnsoppgjor(ansatte, kronetillegg);
 		lonnsoppgjor(ansatte, prosenttillegg);
 		lonnsoppgjor(ansatte, kronetilleggLavLonn);
 		lonnsoppgjor(ansatte, prosenttilleggHvisMann);
-		
+
 		skrivUtAlle(ansatte);
 	}
-	
+
 	static Function<Ansatt, Integer> kronetillegg = a -> a.setLonn(a.getLonn() + 50000);
 	static Function<Ansatt, Integer> prosenttillegg = a -> a.setLonn((int) (a.getLonn() * 1.05));
 	static Function<Ansatt, Integer> kronetilleggLavLonn = a -> a.getLonn() < 500000 ? kronetillegg.apply(a) : 0;
-	static Function<Ansatt, Integer> prosenttilleggHvisMann = a -> a.getKjonn().equals(Kjonn.MANN) ? prosenttillegg.apply(a) : 0;
-	
+	static Function<Ansatt, Integer> prosenttilleggHvisMann = a -> a.getKjonn().equals(Kjonn.MANN)
+																? prosenttillegg.apply(a)
+																: 0;
+
 	public static void lonnsoppgjor(List<Ansatt> ansatte, Function<Ansatt, Integer> f) {
 		for (Ansatt a : ansatte)
 			f.apply(a);
 	}
-	
+
 	public static void skrivUtAlle(List<Ansatt> ansatte) {
 		for (Ansatt a : ansatte)
 			System.out.println(a.toString());
--- src/no/hvl/dat108/oppgave3/Oppg3.java
+++ src/no/hvl/dat108/oppgave3/Oppg3.java
@@ -1,6 +1,7 @@
 package no.hvl.dat108.oppgave3;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,50 +10,55 @@ import java.util.stream.IntStream;
 public class Oppg3 {
 
 	public static void main(String[] args) {
-		
+
 		List<Ansatt> ansatte = new ArrayList<Ansatt>();
-		
+
 		ansatte.add(new Ansatt("Mats", "Ramstad", "Kaptein", Kjonn.MANN, 725000));
 		ansatte.add(new Ansatt("Andreas", "Sannesmoen", "Vaktmester", Kjonn.MANN, 1000000));
 		ansatte.add(new Ansatt("Matilde", "Knarvik", "Matros", Kjonn.KVINNE, 450000));
-		ansatte.add(new Ansatt("Henrik", "Hammer", "Sjef for memes", Kjonn.MANN, 550000));
+		ansatte.add(new Ansatt("Henrik", "Hammer", "Sjef for memes", Kjonn.MANN, 450000));
 		ansatte.add(new Ansatt("Andrea", "Sannesmoen", "Sjef for økomoni", Kjonn.KVINNE, 650000));
-		
-		//a
+
+		// a
 		List<String> etternavn = ansatte.stream().map(a -> a.getEtternavn()).collect(Collectors.toList());
 		System.out.println(etternavn.toString());
 		System.out.println();
-		
-		//b
+
+		// b
 		int antallKvinner = (int) ansatte.stream().filter(a -> a.getKjonn().equals(Kjonn.KVINNE)).count();
 		System.out.printf("Antall kvinner: %d\n", antallKvinner);
 		System.out.println();
-		
-		//c
+
+		// c
 		int lonnKvinner = (int) ansatte.stream().filter(a -> a.getKjonn().equals(Kjonn.KVINNE))
 										.mapToInt(a -> a.getLonn()).average().getAsDouble();
-		//int lonnKvinner = (int) ansatte.stream().mapToInt(a -> a.getKjonn().equals(Kjonn.KVINNE) ? a.getLonn() : 0).average().getAsDouble();
 		System.out.printf("Gjennomsnittslønn hos kvinner: %d\n", lonnKvinner);
-		
-		//d
+
+		// d
 		ansatte.stream().filter(a -> a.getStilling().contains("sjef")).forEach(a -> System.out.println(a.toString()));
 		System.out.println();
-		
-		//e
+
+		// e
 		boolean b = ansatte.stream().anyMatch(a -> a.getLonn() > 800000);
 		System.out.printf("Tjener noen mer enn 800 000kr? %b\n", b);
 		System.out.println();
-		
-		//f
+
+		// f
 		ansatte.stream().forEach(a -> System.out.println(a.toString()));
 		System.out.println();
+
+		// g
+		int minsteLonn = ansatte.stream().mapToInt(Ansatt::getLonn).min().getAsInt();
+		List<Ansatt> minsteLonnAnsatte = ansatte.stream().filter(a -> a.getLonn() == minsteLonn).collect(Collectors.toList());
 		
-		//g
-		Ansatt minstLonn = ansatte.stream().min(Comparator.comparing(Ansatt::getLonn)).get();
-		System.out.printf("Ansatt med lavest lønn er %s %s\n", minstLonn.getFornavn(), minstLonn.getEtternavn());
+		System.out.println("De minste tjenende ansatte er:");
+		
+		for (Ansatt a : minsteLonnAnsatte)
+			System.out.println(a.toString());
+		
 		System.out.println();
-		
-		//h
+
+		// h
 		int sum = IntStream.range(1, 1000).filter(i -> i % 3 == 0 || i % 5 == 0).sum();
 		System.out.printf("Summen av tall i intervallet [1, 1000> som er delelig med 3 eller 5 er %d\n", sum);
 	}
